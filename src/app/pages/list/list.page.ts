ca0dc05ea861aaa6a659cd51a2d997e11aaee500
Alejandro <alexsotog82@gmail.com>
2019-06-11 18:11:04 -0500

    resguardo

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Req02Service } from 'src/app/services/req02.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPage implements OnInit {
 
-  constructor() { }
+  req02 ={
+    empresa: '',
+    departamentoSolicitante: '',
+    quienSolicita: '',
+    marca: '',
+    modelo: '',
+    serie: '',
+    contrasenaEquipo: '',
+    falla: ''
+  }
+
+  constructor(private req02Service: Req02Service) { }
 
   ngOnInit() {
+  }
+
+  onSumitReparacion(){
+    this.req02Service.createReq02(this.req02).subscribe((data:any)=>{
+      console.log(data);
+    })
   }
 
 }
