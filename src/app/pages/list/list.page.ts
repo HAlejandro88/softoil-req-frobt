6758bc4c6d6b6f3826ebc1065ac8d49ae3ad4556
Alejandro <alexsotog82@gmail.com>
2019-06-12 17:40:49 -0500

    se sube modulo completo

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Req02Service } from 'src/app/services/req02.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-list',
@@ -11,23 +12,55 @@ export class ListPage implements OnInit {
   req02 ={
     empresa: '',
     departamentoSolicitante: '',
-    quienSolicita: '',
+    nombreSolicitante: '',
     marca: '',
     modelo: '',
     serie: '',
     contrasenaEquipo: '',
-    falla: ''
+    descripcionFallo: ''
   }
 
-  constructor(private req02Service: Req02Service) { }
+  constructor(private req02Service: Req02Service, private alertController: AlertController) { }
 
   ngOnInit() {
   }
 
   onSumitReparacion(){
-    this.req02Service.createReq02(this.req02).subscribe((data:any)=>{
-      console.log(data);
-    })
+    this.presentAlertConfirm();
+  }
+
+  async presentAlertConfirm() {
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: `<p>Empresa: <strong>${this.req02.empresa}</strong></p>
+                <p>Departamento que lo Solicita: <strong>${this.req02.departamentoSolicitante}</strong></p>
+                <p>Nombre del Solicitante : <strong>${this.req02.nombreSolicitante}</strong></p>
+                <p>Marca: <strong>${this.req02.marca}</strong></p>
+                <p>Modelo: <strong>${this.req02.modelo}</strong></p>
+                <p>Serie: <strong>${this.req02.serie}</strong></p>
+                <p>Contraseña del Equipo: <strong>${this.req02.contrasenaEquipo}</strong></p>
+                <p>Falla: <strong>${this.req02.descripcionFallo}</strong></p>`,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('cancelar');
+          }
+        }, {
+          text: 'Okay',
+          handler: () => {
+            console.log('Confirm Okay');
+            this.req02Service.createReq02(this.req02).subscribe((data:any)=>{
+              console.log(data);
+            })
+          }
+        }
+      ]
+    });
+
+    await alert.present();
   }
 
 }
